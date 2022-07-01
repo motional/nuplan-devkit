3ed3cc5ca948f17b3a8f6d0df01982c73f4faca0
Pat Karnchanachari <pat.karnchanachari@motional.com>
2022-07-01 18:05:20 +0800

    Use conda to install requirements

--- setup.py
+++ setup.py
@@ -6,22 +6,15 @@ import setuptools
 script_folder = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_folder)
 
-# Get requirements
-requirements_path = os.path.join(script_folder, 'requirements.txt')
-requirements = []
-with open(requirements_path, 'r') as f:
-    requirements += f.read().splitlines()
-
 # Installs
 setuptools.setup(
     name='nuplan-devkit',
-    version='0.2.0',
+    version='1.1.0',
     author='The nuPlan team @ Motional',
     author_email='nuscenes@motional.com',
     description='The official devkit of the nuPlan dataset (www.nuPlan.org).',
     url='https://github.com/motional/nuplan-devkit',
     python_requires='>=3.9',
-    install_requires=requirements,
     packages=setuptools.find_packages(script_folder, exclude=['*test', 'bazel-nuplan_devkit*', 'bazel-nuplan-devkit*']),
     package_data={'': ['*.json']},
     include_package_data=True,
